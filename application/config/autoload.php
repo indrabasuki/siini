8865439cea7285351b4eb809b0958b6da0f5acff
siksik-sys <skripsiinformatika6@gmail.com>
2021-02-25 15:26:33 +0700

    upload

---
+++ application/config/autoload.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+
+$autoload['packages'] = array();
+$autoload['libraries'] = array();
+$autoload['drivers'] = array();
+$autoload['helper'] = array();
+$autoload['config'] = array();
+$autoload['language'] = array();
+$autoload['model'] = array();
